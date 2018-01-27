a4a095f863f60434ef1aa620c6980388f9f04ba7
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-01-26 17:24:23 -0800

    bio page complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 'use strict';
 
+$('.logo').click(event => {
+  event.preventDefault();
+  $('.js-landing-page').removeAttr('hidden');
+  $('#footer').removeAttr('hidden');
+  $('.js-bio-page').attr('hidden', true);
+  $('.js-portfolio-page').attr('hidden', true);
+})
+
+$('.js-bio-link').click(event => {
+  event.preventDefault();
+  $('.js-landing-page').attr('hidden', true);
+  $('#footer').attr('hidden', true);
+  $('.js-bio-page').removeAttr('hidden');
+  $('.js-portfolio-page').attr('hidden', true);
+});
+
+$('.js-portfolio-link').click(event => {
+  event.preventDefault();
+  $('.js-landing-page').attr('hidden', true);
+  $('#footer').attr('hidden', true);
+  $('.js-bio-page').attr('hidden', true);
+  $('.js-portfolio-page').removeAttr('hidden');
+});
