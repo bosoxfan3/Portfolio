0409284b3c38d3eaeabb334bbffce9df5f924a8e
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-02-08 13:37:13 -0800

    trying to fix issue with bio and portfolio links

--- index.js
+++ index.js
@@ -77,24 +77,21 @@ $('.js-bored-button').click(event => {
   }
 });
 
-$('.logo').click(event => {
-  event.preventDefault();
+$('.logo').click(() => {
   $('.js-landing-page').removeAttr('hidden');
   $('#footer').removeAttr('hidden');
   $('.js-bio-page').attr('hidden', true);
   $('.js-portfolio-page').attr('hidden', true);
 });
 
-$('.js-bio-link').click(event => {
-  event.preventDefault();
+$('.js-bio-link').click(() => {
   $('.js-landing-page').attr('hidden', true);
   $('#footer').attr('hidden', true);
   $('.js-bio-page').removeAttr('hidden');
   $('.js-portfolio-page').attr('hidden', true);
 });
 
-$('.js-portfolio-link').click(event => {
-  event.preventDefault();
+$('.js-portfolio-link').click(() => {
   $('.js-landing-page').attr('hidden', true);
   $('#footer').attr('hidden', true);
   $('.js-bio-page').attr('hidden', true);
